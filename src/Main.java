6001cba4be0ac8ae4425ecb9d69ab7cef5dec40c
Timothy C. Wright <tcw321@gmail.com>
2018-10-07 17:15:05 -0400

    added a public method to take an int and set the private field value. On my laptop in master. This is an independent change to the change made on github also on master

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,13 @@
 public class Main {
 
+    private int m_b;
+
     public static void main(String[] args) {
         System.out.println("Hello World!");
+    }
+
+    public void func(int a)
+    {
+        m_b = a;
     }
 }
